d6c5d2102f46da2124b02e38da9e7153e92def23
Djambek Tlepbergenov <d.d.tlepbergenov@gmail.com>
2022-04-30 22:53:43 +0300

    Точно работает теперь можно сюда код переписывать

--- app/src/main/java/com/example/courts/HomeFragment.java
+++ app/src/main/java/com/example/courts/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +16,14 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ListView listview = view.findViewById(R.id.listview);
+        Integer[] i = new Integer[100];
+        for (int j = 0; j < 100; j++) {
+            i[j] = j;
+        }
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getContext(), android.R.layout.simple_list_item_1, i);
+        listview.setAdapter(adapter);
+        return view;
     }
 }
